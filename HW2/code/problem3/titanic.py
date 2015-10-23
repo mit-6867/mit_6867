b85399d19c6da828a45988a368d5247e13264528
mfzhao <mfzhao@mit.edu>
2015-10-23 12:30:04 -0400

    Made a seperate file for just the SVM stuff, still not working

--- HW2/code/problem3/titanic.py
+++ HW2/code/problem3/titanic.py
@@ -4,12 +4,9 @@ import matplotlib as mpl
 from ggplot import *
 import pandas as pd
 import scipy.io
-import cvxopt
-from cvxopt import matrix
-from problem2 import *
 
 def scaling(X):
-    for i in (4,5,6,7):
+    for i in range(11):
         xmax = max(X[:,i])
         xmin = min(X[:,i])
         X[:,i] =  (X[:,i] - xmin)/(xmax - xmin)
@@ -24,7 +21,7 @@ def LRPredict(w, X):
     
 # Training Function
 def Train(X, Y, l=0):
-    w0 = random.randn(13)
+    w0 = random.randn(12)
     w_opt = scipy.optimize.fmin_bfgs(LRLoss, w0, args=(X,Y,l))
     return w_opt
 
@@ -43,12 +40,10 @@ def loadTitan(name, typ):
         data = scipy.io.loadmat('/Users/dholtz/Downloads/hw2_resources/data/'+file)['data']
     except:
         data = scipy.io.loadmat('/Users/mfzhao/Downloads/hw2_resources/data/'+file)['data']
-    temp = data[:,0:11]
-    scaling(temp)
+    X = data[:,0:11]
+    scaling(X)
     Y = data[:,11:12]
-    X = ones([len(temp), 12])
-    X[:,1:12] = temp
-    return X.copy(), Y.copy()
+    return X, Y
 
 def GridLR(Xt, Yt, Xv, Yv, l, db=0.5):
     l_opt = 0
@@ -69,21 +64,6 @@ def GridLR(Xt, Yt, Xv, Yv, l, db=0.5):
 def SVMErr(yhat, y):
     return 1 - mean((yhat == y))
 
-def SVMGrid(Xtr, Ytr, Xv, Yv):
-    C = linspace(0,100,101)
-    bestErr = 1
-    kernel = gaussian_kernel
-    bestC = 0
-    for i in range(len(C)):
-        n_features, K, alpha, sv, sv_y, sv_bool, ind = trainSVM(Xtr, Ytr, kernel, C=C[i])
-        yhat = predictSVM(Xv)
-        cErr = SVMErr(yhat, Yv)
-        if cErr < bestErr:
-            bestErr = cErr
-            bestC = C[i]
-            n_featuresb, Kb, alphab, svb, sv_yb, sv_boolb, indb = n_features, K, alpha, sv, sv_y, sv_bool, ind
-    return n_featuresb, Kb, alphab, svb, sv_yb, sv_boolb, indb, bestC
-
 Xtr, Ytr= loadTitan('titanic', 'train')
 Xv, Yv= loadTitan('titanic', 'validate')
 Xtest, Ytest = loadTitan('titanic', 'test')
@@ -102,29 +82,6 @@ print 'LR Training Classification Error: ',CE_LRtr
 print 'LR Validation Classification Error: ',CE_LRtr
 print 'LR Test Classification Error: ',CE_LRtr
 print '===================================================='
-kernel = gaussian_kernel
-#n_features, K, alpha, sv, sv_y, sv_bool, ind = trainSVM(Xtr, Ytr, kernel, 1)
-n_features, K, alpha, sv, sv_y, sv_bool, ind = SVMGrid(Xtr, Ytr, Xv, Yv)
-yhattr = predictSVM(XtrC)
-yhatv = predictSVM(XvC)
-yhattest = predictSVM(XtestC)
-CE_SVMtr = SVMErr(yhattr, YtrC)
-CE_SVMv = SVMErr(yhatc, YtrC)
-CE_SVMtest = SVMErr(yhattest, YtrC)
-
-w_SVM = np.zeros(n_features)
-for n in range(len(alpha)):
-    w_SVM += alpha[n] * sv_y[n] * sv[n]
-
-print '=========================SVM========================='
-print 'optimal w:', w_SVM
-
-print 'SVM Training Classification Error: ',CE_LRtr
-print 'SVM Validation Classification Error: ',CE_LRtr
-print 'SVM Test Classification Error: ',CE_LRtr
-
-print 'geometric margin: ', geometricMarginSVM()
-print '====================================================='
 
 
 
