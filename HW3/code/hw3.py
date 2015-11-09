2546a91ef748fb26876e2de15c43fd53cc722e97
Dave Holtz <davemholtz@gmail.com>
2015-11-09 00:26:46 -0500

    [nn] ugh.

--- HW3/code/hw3.py
+++ HW3/code/hw3.py
@@ -26,15 +26,15 @@ def forwardProp(X_train, w1, w2, activation_functions):
     activations = activation_functions[0](output)
     output2 = np.dot(activations, w2[1:,:]) + w2[0:1,:]
     activations2 = activation_functions[1](output2)
-    return activations2
+    return output, activations, output2, activations2
 
 def lossFunction(output, Y_train, l, w1, w2):
 	Y_train_matrix = np.zeros((Y_train.shape[0], len(np.unique(Y_train))))
 	Y_train_locations = np.hstack([np.arange(Y_train.shape[0]).reshape(-1, 1), Y_train-1])
 	Y_train_matrix[Y_train_locations[:,0:1].astype(int), Y_train_locations[:,1:].astype(int)] = 1
-	loss = np.sum(-Y_train_matrix*np.log(output) - (1-Y_train_matrix)*np.log(1-output)) + l*(np.sum(w1**2) + np.sum(w2**2))
+	loss = np.sum(-Y_train_matrix*np.log(output) - (1-Y_train_matrix)*np.log(1-output)) + l*(np.sum(w1[1:,:]**2) + np.sum(w2[1:,:]**2))
 	print loss
-	return loss
+	return loss, Y_train_matrix
 
 def neuralNetwork(X_train, Y_train, n1, n2, l, activation_functions, learning_rate):
     w1 = 2*np.random.random((X_train.shape[1]+1,n1)) - 1
@@ -43,8 +43,29 @@ def neuralNetwork(X_train, Y_train, n1, n2, l, activation_functions, learning_ra
     MSE = () 
     error_limit = 1e-3
     while MSE > error_limit:
-    	output = forwardProp(X_train, w1, w2, activation_functions)
-    	error = lossFunction(output, Y_train, l, w1, w2)
+    	output, activations, output2, activations2 = forwardProp(X_train, w1, w2, activation_functions)
+    	error, Y_train_matrix = lossFunction(activations2, Y_train, l, w1, w2)
+
+    	corrections2 = Y_train_matrix*(1-activations2) + (1-Y_train_matrix)*activations2
+    	activationsBias2 = np.hstack([activations, np.ones((activations.shape[0], 1)).reshape(-1, 1)])
+    	errorTerm2 = np.dot(np.transpose(activationsBias2), corrections2)
+    	errorTermReg2 = errorTerm2 + 2*l*w2
+        print errorTermReg2
+
+
+        X_trainBias = np.hstack([X_train, np.ones((X_train.shape[0], 1)).reshape(-1, 1)])
+        activationsBias = np.hstack([activations, np.ones((activations.shape[0], 1)).reshape(-1,1)])
+        activationDeriv = sigmoidFunction(output, derivative=True)
+        activationDerivBias = np.hstack([activationDeriv, np.ones((activationDeriv.shape[0], 1)).reshape(-1,1)])
+
+        print X_trainBias.shape
+        print X_train.shape
+        print corrections2.shape
+        print w2.shape
+        print np.dot(np.dot(np.transpose(np.dot(corrections2, np.transpose(w2))), activations), np.transpose(activationDeriv)).shape
+        print activationDeriv.shape
+        print w1.shape
+
 	
 
-neuralNetwork(X_train, Y_train, 4, 3, .1, [sigmoidFunction, sigmoidFunction])
+neuralNetwork(X_train, Y_train, 4, 3, .1, [sigmoidFunction, sigmoidFunction], .3)
