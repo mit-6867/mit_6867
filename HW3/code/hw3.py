6d7df5791137699f0445cfa8bc1e5e1be2ce72bc
Dave Holtz <davemholtz@gmail.com>
2015-11-09 09:57:33 -0500

    [hw3] nn works, does poorly.

--- HW3/code/hw3.py
+++ HW3/code/hw3.py
@@ -41,16 +41,18 @@ def neuralNetwork(X_train, Y_train, n1, n2, l, activation_functions, learning_ra
     w2 = 2*np.random.random((w1.shape[1]+1, n2)) - 1 
     
     MSE = () 
-    error_limit = 1e-3
-    while MSE > error_limit:
+    c = 1e-5
+    error = 10000
+    old_error = 1000000
+    while np.abs(old_error-error) > c:
+    	old_error = error
     	output, activations, output2, activations2 = forwardProp(X_train, w1, w2, activation_functions)
     	error, Y_train_matrix = lossFunction(activations2, Y_train, l, w1, w2)
 
     	corrections2 = Y_train_matrix*(1-activations2) + (1-Y_train_matrix)*activations2
     	activationsBias2 = np.hstack([activations, np.ones((activations.shape[0], 1)).reshape(-1, 1)])
     	errorTerm2 = np.dot(np.transpose(activationsBias2), corrections2)
-    	errorTermReg2 = errorTerm2 + 2*l*w2
-        print errorTermReg2
+    	errorTermReg2 = errorTerm2/float(X_train.shape[0]) + 2*l*w2
 
 
         X_trainBias = np.hstack([X_train, np.ones((X_train.shape[0], 1)).reshape(-1, 1)])
@@ -58,14 +60,28 @@ def neuralNetwork(X_train, Y_train, n1, n2, l, activation_functions, learning_ra
         activationDeriv = sigmoidFunction(output, derivative=True)
         activationDerivBias = np.hstack([activationDeriv, np.ones((activationDeriv.shape[0], 1)).reshape(-1,1)])
 
-        print X_trainBias.shape
-        print X_train.shape
-        print corrections2.shape
-        print w2.shape
-        print np.dot(np.dot(np.transpose(np.dot(corrections2, np.transpose(w2))), activations), np.transpose(activationDeriv)).shape
-        print activationDeriv.shape
-        print w1.shape
+        XtimesCorrections = np.dot(np.transpose(X_trainBias), corrections2)
+        productTimesW2 = np.dot(XtimesCorrections, np.transpose(w2[1:,:]))
+        errorTermReg1 = np.dot(np.dot(productTimesW2, np.transpose(activations)), activationDeriv)/float(X_train.shape[0]) + 2*l*w1
 
-	
+        w1 = w1 - learning_rate*errorTermReg1
+        w2 = w2 - learning_rate*errorTermReg2
 
-neuralNetwork(X_train, Y_train, 4, 3, .1, [sigmoidFunction, sigmoidFunction], .3)
+    return w1, w2
+
+def nnPredict(X_train, w1, w2, activation_functions):
+	output, activations, output2, Y_matrix = forwardProp(X_train, w1, w2, activation_functions)
+	print Y_matrix
+	maxClass = np.argmax(Y_matrix, axis=1)
+	return maxClass
+
+def classificationError(yPredict, Y):
+	classificationError = np.sum(np.array(yPredict.reshape(-1, 1) == Y.reshape(-1, 1), dtype=bool))/Y.shape[0]
+	print classificationError
+	return classificationError
+
+w1, w2 = neuralNetwork(X_train, Y_train, 10, 3, .01, [sigmoidFunction, sigmoidFunction], .00005)
+
+yPredict = nnPredict(X_train, w1, w2, [sigmoidFunction, sigmoidFunction])
+
+classificationError(yPredict+1, Y_train)
