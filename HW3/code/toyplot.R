1dc3acf2057e03d02169e5a73745e21012eb3b5d
Dave Holtz <davemholtz@gmail.com>
2015-11-10 23:48:36 -0500

    [hw3] final draft.

--- HW3/code/toyplot.R
+++ HW3/code/toyplot.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(scales)
 
 toy_test_1 <- read.csv('~/Downloads/toy_multiclass_1_testCLEAN.csv', head=T)
 toy_validate_1 <- read.csv('~/Downloads/toy_multiclass_1_validateCLEAN.csv', head=T)
@@ -31,11 +32,22 @@ toy_2_complete <- rbind(toy_validate_2 %>%
 
 t1 <- ggplot(toy_1_complete, aes(x=as.numeric(toy.data.1), y=as.numeric(toy.data.2), 
                        color=Class, shape=Dataset)) + geom_point() + xlab('X1') + ylab('X2') + 
-  ggtitle('Distribution of Toy Data Set 1 in 2-dimensions')
+  ggtitle('Scatterplot of Toy Data Set 1 in 2-dimensions')
 ggsave(t1, file='../tex/t1dist.jpg')
 
 t2 <- ggplot(toy_2_complete, aes(x=as.numeric(toy.data.1), y=as.numeric(toy.data.2), 
                            color=Class, shape=Dataset)) + geom_point() + xlab('X1') + ylab('X2') +
-  ggtitle('Distribution of Toy Data Set 2 in 2-dimensions')
-ggsave(t2, file='t2dist.jpg')
+  ggtitle('Scatterplot of Toy Data Set 2 in 2-dimensions')
+ggsave(t2, file='../tex/t2dist.jpg')
 
+nn_performance <- read.csv('nnperformance.csv', head=T)
+head(nn_performance)
+
+nnperf <- nn_performance %>% 
+  mutate(`Learning Rate` = as.factor(lr),
+         `Regularization` = as.factor(lambda)) %>%
+  ggplot(aes(x=n, y=error, color=`Regularization`)) + 
+  geom_line(aes(linetype=`Learning Rate`), size=1) + scale_y_continuous(labels=percent) + facet_wrap(~method) + 
+  xlab('Hidden Units') + ylab('Classification Error Rate') + 
+  ggtitle('Neural Network Classification Error on MNIST Validation Dataset')
+ggsave(nnperf, file='../tex/nnperf.jpg')
