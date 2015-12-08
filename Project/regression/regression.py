4f1110005520bacd2ca4dd09f052010ef9707deb
Dave Holtz <davemholtz@gmail.com>
2015-12-07 23:11:12 -0500

    [project] regression, MSE

--- Project/regression/regression.py
+++ Project/regression/regression.py
@@ -1,17 +1,17 @@
 import pandas as pd 
 import pickle 
-
+import numpy as np 
 
 def create_dummy_variables(df, columns):
-	dummies = pd.get_dummies(articleData[columns], prefix=columns)
-	df_reduced = df.ix[:, df.columns - columns]
-	new_df = pd.concat([df_reduced, dummies], axis=1)
-	return new_df
+    dummies = pd.get_dummies(articleData[columns], prefix=columns)
+    df_reduced = df.ix[:, df.columns - columns]
+    new_df = pd.concat([df_reduced, dummies], axis=1)
+    return new_df
 
 def clean_up_whitespace(string):
-	while '  ' in string:
-		string = string.replace('  ', ' ')
-	return string	.encode('utf-8', 'ignore')
+    while '  ' in string:
+        string = string.replace('  ', ' ')
+    return string    .encode('utf-8', 'ignore')
 
 
 
@@ -28,42 +28,42 @@ bigramPerplexity = pickle.load(open('bigram-perplexity.pkl', 'rb'))
 articleData = pd.concat([articleData, bigramPerplexity], axis=1)
 
 def get_author_gender(author_array):
-	genders = []
+    genders = []
 
-	if len(author_array) > 0:
-		for i in author_array:
-			j = clean_up_whitespace(i)
-			prop_male = authorData[authorData['author_name'] == j]['prop_male']
-			if min(prop_male) >= .9:
-				genders += [1.] 
-			elif max(prop_male) <= .1:
-				genders += [0.] 
-			else: 
-				genders += [.5]
+    if len(author_array) > 0:
+        for i in author_array:
+            j = clean_up_whitespace(i)
+            prop_male = authorData[authorData['author_name'] == j]['prop_male']
+            if min(prop_male) >= .9:
+                genders += [1.] 
+            elif max(prop_male) <= .1:
+                genders += [0.] 
+            else: 
+                genders += [.5]
 
-	else: 
-		genders += [.5]
+    else: 
+        genders += [.5]
 
-	if max(genders) == 1. and min(genders) == 1.:
-		return 'male'
-	elif min(genders) == 0. and max(genders) == 0.:
-		return 'female'
-	else:
-		return 'ambiguous / unknown'
+    if max(genders) == 1. and min(genders) == 1.:
+        return 'male'
+    elif min(genders) == 0. and max(genders) == 0.:
+        return 'female'
+    else:
+        return 'ambiguous / unknown'
 
 def get_author_popularity(author_array):
-	popularities = []
+    popularities = []
 
-	if len(author_array) > 0:
-		for i in author_array:
-			j = clean_up_whitespace(i)
-			popularity = authorData[authorData['author_name'] == j]['popularity'].tolist()
-			popularities += popularity
+    if len(author_array) > 0:
+        for i in author_array:
+            j = clean_up_whitespace(i)
+            popularity = authorData[authorData['author_name'] == j]['popularity'].tolist()
+            popularities += popularity
 
-	else: 
-		popularities += [int(authorData['popularity'].median())]
+    else: 
+        popularities += [int(authorData['popularity'].median())]
 
-	return sum(popularities) / float(len(popularities))
+    return sum(popularities) / float(len(popularities))
 
 
 articleData['author_gender'] = articleData.authors.apply(get_author_gender)
@@ -72,5 +72,33 @@ articleData['popularity_pre_log'] = articleData.authors.apply(get_author_popular
 dummyColumns = ['typeOfMaterial', 'desk', 'type', 'section', 'author_gender']
 articleDataDummies = create_dummy_variables(articleData, dummyColumns)
 articleDataDummies = articleDataDummies.merge(fleschKincaid, on=['id'])
+articleDataDummies['flesch-kincaid_score'].fillna(articleDataDummies['flesch-kincaid_score'].median(), inplace=True)
 
-print articleDataDummies
+articleDataDummies['log_count'] = articleDataDummies['count'].apply(np.log)
+articleDataDummies['log_popularity'] = articleDataDummies.popularity_pre_log.apply(np.log)
+
+columnstoRemove = ['authors', 'desFacet', 'geoFacet', 'headline', 'id', 'orgFacet', 'perFacet', 'publishedDate', 'count', 'popularity_pre_log', 'log_count']
+articleDataDummiesRegression = articleDataDummies.ix[:, articleDataDummies.columns - columnstoRemove]
+yValues = articleDataDummies['log_count']
+
+X = articleDataDummiesRegression.values
+y = yValues.values
+
+XtX = np.dot(np.transpose(X), X)
+XtXinv = np.linalg.pinv(XtX)
+
+beta = np.dot(np.dot(XtXinv, np.transpose(X)), y)
+
+def predict(beta, X):
+    return np.dot(np.reshape(beta, (1, -1)), np.transpose(X))
+
+def MSE(predictions, actuals):
+	print np.reshape(actuals, (1, -1)).shape
+	print predictions.shape
+	mse = np.dot((np.reshape(actuals, (1, -1))-predictions), np.transpose(np.reshape(actuals, (1, -1))-predictions))/float(predictions.size)
+
+	return mse 
+
+yPredict = predict(beta, X)
+
+print MSE(yPredict, y)
