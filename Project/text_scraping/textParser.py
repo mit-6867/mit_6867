c458a49252d3f06f725c11b5282f238fb1827253
Michael Zhao <mfzhao@mit.edu>
2015-12-07 19:14:03 -0500

    textParser fixes

--- Project/text_scraping/textParser.py
+++ Project/text_scraping/textParser.py
@@ -1,20 +1,37 @@
 from newspaper import Article
-import pandas as pd 
+import sys
+import re
+import codecs
 
-article_urls = pd.read_csv('article_list.txt', header=None)
-url = 'http://www.nytimes.com/2013/08/31/sports/ncaafootball/lewan-staying-at-michigan-with-renewed-focus.html'
+blk = int(sys.argv[1])
 
-for i in range(len(article_urls)):
-	print i 
-	article = Article(url=article_urls[0][i])
-	article.download()
-	article.parse()
-	text = article.text
-	text = text.replace('Photo\n', '')
-	text = text.replace('Photo \n', '')
-	text = text.replace('Advertisement Continue reading the main story\n', '')
-	text = text.replace('\n\n', ' ')
-	text = text[1:]
-	f = open('article_text/' + str(i + 1) + '.txt', 'w')
-	f.write(text.encode('utf8'))
-	f.close()
+article_urls = codecs.open('list_fixed.txt', encoding = 'utf-8').read().splitlines()
+
+def wrapper(block):
+    for i in block:
+        print i 
+        article = Article(url=article_urls[i])
+        article.download()
+        article.parse()
+        text = article.text
+        text = text.replace('Photo\n', '')
+        text = text.replace('Photo \n', '')
+        text = text.replace('Advertisement Continue reading the main story\n', '')
+        text = text.replace('Photo Advertisement Continue reading the main story\n', '')
+        text = re.sub("\n+" , " ", text)
+        text = re.sub('^ ', '', text)
+        f = open('article_text/' + str(i + 1) + '.txt', 'w')
+        f.write(text.encode('utf-8'))
+        f.close()
+
+if blk == 1:
+    wrapper(xrange(1670))
+    
+elif blk == 2:
+    wrapper(xrange(1670, 3341))
+    
+elif blk == 3:
+    wrapper(xrange(3341,5011))
+    
+elif blk == 4:
+    wrapper(xrange(5011,6682))
