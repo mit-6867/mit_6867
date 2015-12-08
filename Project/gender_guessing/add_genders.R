b4902e6b7c26c0367e0006f1d1d82cec53a3ce23
Jeremy Yang <jeremy.z.yang@hotmail.com>
2015-12-08 18:41:48 -0500

    Update add_genders.R

--- Project/gender_guessing/add_genders.R
+++ Project/gender_guessing/add_genders.R
@@ -27,3 +27,55 @@ authors$prop_female <- as.numeric(authors$prop_female)
 
 write.table(authors, file='authors_with_genders.tsv', row.names=FALSE, sep="\t", 
             quote=FALSE, fileEncoding='UTF-8')
+
+
+# split the name string to get multiple author names 
+
+n <- str_split(authors$names,'AND ')
+
+authors_number <- vector()
+
+for (i in 1:7367){
+  print (i)
+  authors_number[i] <- length(n[[i]])
+}
+max(authors_number)
+
+author_1_first_name <- vector()
+author_2_first_name <- vector()
+
+for (i in 1:7367){
+  print (i)
+  if(n[[i]]==''){
+    author_1_first_name[i] = 'NA'
+    author_2_first_name[i] = 'NA'
+  } else if (length(n[[i]])==1){
+      author_1_first_name[i] = first_name(n[[i]])
+      author_2_first_name[i] = 'NA'
+    } else {
+    author_1_first_name[i] = first_name(n[[i]][1])
+    author_2_first_name[i] = first_name(n[[i]][2])
+  }
+}
+
+authors <- cbind(authors,author_1_first_name,author_2_first_name)
+
+colnames(authors)[5] <- 'prop_male_1'
+colnames(authors)[6] <- 'prop_female_1'
+
+critical <- .5
+authors$gender_1 <- rep('NA',nrow(authors))
+authors$gender_2 <- rep('NA',nrow(authors))
+
+authors$gender_1[which(authors$prop_male_1=='NA')] <- 'NA'
+authors$gender_1[which(authors$prop_male_1!='NA' & authors$prop_male_1>=critical)] <- '0'
+authors$gender_1[which(authors$prop_male_1!='NA' & authors$prop_female_1 >= critical)] <- '1'
+
+authors$gender_2[which(authors$prop_male_2=='NA')] <- 'NA'
+authors$gender_2[which(authors$prop_male_2!='NA' & authors$prop_male_2>=critical)] <- '0'
+authors$gender_2[which(authors$prop_male_2!='NA' & authors$prop_female_2 >= critical)] <- '1'
+
+table(authors$gender_1)
+table(authors$gender_2)
+
+write.csv(authors,'multiple_authors.csv',row.names = F)
