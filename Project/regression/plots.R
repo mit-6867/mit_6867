d50c27956961c5642e0bfb87fc57e5f64b5a4ec2
Dave Holtz <davemholtz@gmail.com>
2015-12-08 23:30:46 -0500

    [project] some new plots.

--- Project/regression/plots.R
+++ Project/regression/plots.R
@@ -1,14 +1,68 @@
 library(ggplot2)
 library(knitr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+setwd('/Users/dholtz/Desktop/mit_6867/Project/Latex/')
 
-weights <- c(-0.0155673547965, -0.032512577208, -0.0183928993822, 0.0436023817917, -0.168735260358, -0.0548502431781, 0.0400864539071, 0.0477745936425, 0.0482044874247, 0.656851318914, 0.101863103466, 0.0787925572846, 0.0772113800894, -0.109357780179, 0.00259863923261, 0.253263211162, 0.00423288154355, -0.151785209635, -0.086932735094, 0.0642537764487, 0.0594296289471, -0.0784431104261, 0.0100914683538, -0.0455614111585, -0.150498327579, 0.0370920476908, -0.471571091079, 0.00575529953804, -0.174645309934, -0.00122143569646, -0.120772053981, 0.729336915393, 0.0191174119751, -0.0296600316436, 0.0414784388088, 0.192408869835, 0.0525969201364, -0.0430805415879, -0.179258076024, -0.13951006772, 0.0400864539072, -0.199291009302, 0.125170423372, -0.00107423696482, 0.1722575493, -0.103366129896, -0.0142565167554, -0.0481040689734, -0.277144753309, -0.00275229839806, 0.123937726235, 0.111792685733, 0.261123875719, -0.0202035470324, -0.0667582303632, 0.0161341447383, -0.214593015947, 0.0525950326542, 0.0457459855028, 0.0809072398456, 0.0841238574083, -0.0947854358489, -0.0536801646975, -0.105109365666, -0.553358112213, -0.00518030191328, -0.00801186882178, 0.026662047528, -0.0707252848644, -0.00164194215674, -0.00696646258888, -0.0156368327091, -0.243055275894, -0.0265795567348, 0.0236131786527, 0.126044421422, 0.0661457097531, -0.110683337504, 0.0679989404147, 0.167213483369, -0.0287554883533, 0.0611867349866, -0.0321046144057, -0.255398058467, -0.290623015012, 0.0022774254305, -0.0126711530544, -0.0831671026105, -0.0064248106726, -0.170417879777, 9.04696649847)
-names <- c("author_gender_ambiguous / unknown", "author_gender_female", "desk_Arts&Leisure", "desk_Automobiles", "desk_BookReview", "desk_Culture", "desk_Dining", "desk_EdLife", "desk_Editorial", "desk_Foreign", "desk_Home", "desk_Letters", "desk_Magazine", "desk_Metro", "desk_NODESK", "desk_National", "desk_NewsDesk", "desk_None", "desk_OpEd", "desk_RealEstate", "desk_Science", "desk_Society", "desk_Sports", "desk_Styles", "desk_Summary", "desk_SundayBusiness", "desk_Travel", "desk_Washington", "desk_Weekend", "flesch-kincaid_score", "log_popularity", "log_wcount", "perplexity", "section_Automobiles", "section_Blogs", "section_Books", "section_Booming", "section_Business Day", "section_Corrections", "section_Crosswords & Games", "section_Dining & Wine", "section_Education", "section_Fashion & Style", "section_Great Homes and Destinations", "section_Health", "section_Home & Garden", "section_Job Market", "section_Magazine", "section_Movies", "section_Multimedia", "section_Multimedia/Photos", "section_N.Y. / Region", "section_Opinion", "section_Public Editor", "section_Real Estate", "section_Science", "section_Sports", "section_Style", "section_Sunday Review", "section_T Magazine", "section_Technology", "section_Theater", "section_Travel", "section_U.S.", "section_World", "section_Your Money", "typeOfMaterial_An Appraisal", "typeOfMaterial_Biography", "typeOfMaterial_Brief", "typeOfMaterial_Correction", "typeOfMaterial_Economic Analysis", "typeOfMaterial_Editorial", "typeOfMaterial_Letter", "typeOfMaterial_List", "typeOfMaterial_Military Analysis", "typeOfMaterial_News", "typeOfMaterial_News Analysis", "typeOfMaterial_Obituary", "typeOfMaterial_Obituary (Obit)", "typeOfMaterial_Op-Ed", "typeOfMaterial_Question", "typeOfMaterial_Quote", "typeOfMaterial_Recipe", "typeOfMaterial_Review", "typeOfMaterial_Schedule", "typeOfMaterial_Series", "typeOfMaterial_Summary", "typeOfMaterial_Text", "typeOfMaterial_Web Log", "type_BlogPost", "intercept")
+weights <- c(-0.00191432502654, -0.0245399612398, -0.0150996164294, 0.0440296750968, -0.167166767298, -0.0396022800434, 0.0513236665953, 0.0376881505235, 0.034420292747, 0.560583168704, 0.135760449819, 0.119415176685, 0.0976386412178, -0.109717531939, 0.000263172714949, 0.245491206079, 0.00520118309151, -0.165878067649, -0.0892505580431, 0.0776715694698, 0.0461934763289, -0.177856267015, 0.0250636545508, -0.123728689152, -0.146765225723, 0.0466043122084, -0.477516203847, 0.00122058106983, -0.124056938997, 0.00101097778077, -0.0958951336039, 0.735969046607, 0.0167414635067, -0.025959165804, 0.040895009896, 0.219165180421, 0.108392843047, -0.0230247100238, -0.167360937146, -0.143143446175, 0.0513236665944, -0.13857883042, 0.274650293308, -0.00203664259554, 0.20002254626, -0.110278613843, -0.015642686809, -0.0503790999032, -0.267093153066, 0.00630394482605, 0.13920691866, 0.142020776932, 0.244810158572, -0.0206936510913, -0.0641111765411, 0.0342515812982, -0.190922375506, 0.0507730247675, 0.0552535755133, 0.074599895454, 0.0932262989123, -0.0826574211138, -0.0541942203966, -0.0675520953361, -0.409982572288, -0.00532750280566, 0.238705410021, 0.156883824326, 0.163951257457, -0.0177637990337, 0.0234011367299, -0.153357482844, -0.0168654857936, 0.00441377186855, -0.0265800308445, -0.310952228957, -0.0486760700795, 0.0211400997113, 0.00150676823874, 0.0435852536984, -0.00317176562678, 0.0389422324357, 0.15528305309, -0.0603885589553, 0.0301666928586, -0.0452872719984, -0.323507092898, -0.329889081099, -0.00411448761229, -0.0133846358151, -0.0934607743805, -0.00825110975912, -0.394127027181, 0.0491173950399, 0.0599957920327, 0.0289952809853, 0.00144497100566, 0.0541630652262, 0.0187797252149, 9.11388341686)
+names <- c("author_gender_ambiguous / unknown", "author_gender_female", "desk_Arts&Leisure", "desk_Automobiles", "desk_BookReview", "desk_Culture", "desk_Dining", "desk_EdLife", "desk_Editorial", "desk_Foreign", "desk_Home", "desk_Letters", "desk_Magazine", "desk_Metro", "desk_NODESK", "desk_National", "desk_NewsDesk", "desk_None", "desk_OpEd", "desk_RealEstate", "desk_Science", "desk_Society", "desk_Sports", "desk_Styles", "desk_Summary", "desk_SundayBusiness", "desk_Travel", "desk_Washington", "desk_Weekend", "flesch-kincaid_score", "log_popularity", "log_wcount", "perplexity", "section_Automobiles", "section_Blogs", "section_Books", "section_Booming", "section_Business Day", "section_Corrections", "section_Crosswords & Games", "section_Dining & Wine", "section_Education", "section_Fashion & Style", "section_Great Homes and Destinations", "section_Health", "section_Home & Garden", "section_Job Market", "section_Magazine", "section_Movies", "section_Multimedia", "section_Multimedia/Photos", "section_N.Y. / Region", "section_Opinion", "section_Public Editor", "section_Real Estate", "section_Science", "section_Sports", "section_Style", "section_Sunday Review", "section_T Magazine", "section_Technology", "section_Theater", "section_Travel", "section_U.S.", "section_World", "section_Your Money", "timeofday_12-17", "timeofday_18-23", "timeofday_6-11", "typeOfMaterial_An Appraisal", "typeOfMaterial_Biography", "typeOfMaterial_Brief", "typeOfMaterial_Correction", "typeOfMaterial_Economic Analysis", "typeOfMaterial_Editorial", "typeOfMaterial_Letter", "typeOfMaterial_List", "typeOfMaterial_Military Analysis", "typeOfMaterial_News", "typeOfMaterial_News Analysis", "typeOfMaterial_Obituary", "typeOfMaterial_Obituary (Obit)", "typeOfMaterial_Op-Ed", "typeOfMaterial_Question", "typeOfMaterial_Quote", "typeOfMaterial_Recipe", "typeOfMaterial_Review", "typeOfMaterial_Schedule", "typeOfMaterial_Series", "typeOfMaterial_Summary", "typeOfMaterial_Text", "typeOfMaterial_Web Log", "type_BlogPost", "weekday_Monday", "weekday_Saturday", "weekday_Sunday", "weekday_Thursday", "weekday_Tuesday", "weekday_Wednesday", "intercept")
 
 linear_regression_weights = data.frame(weights = weights, names = names)
 
-ggplot(filter(linear_regression_weights, names!='intercept'), aes(x=names, y=weights)) + 
-  geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=90)) + 
-  xlab('Feature') + ylab('Feature Weight')
+linear_regression_weights %>% 
+  mutate(names = str_replace_all(str_replace_all(str_replace_all(
+    str_replace_all(
+      str_replace_all(
+        str_replace_all(
+          str_replace_all(
+            str_replace_all(
+              str_replace_all(
+                str_replace_all(names, 'author_gender_', 'Author Gender: '),
+        'desk_', 'Desk: ')
+    , 'section_', 'Section: '), 'timeofday_', 'Time of Day: '),
+    'typeOfMaterial_', 'Type of Material: '), 'type_', 'Type: '),
+    'weekday_', 'Day of Week: '), 'flesch-kincaid_score', 'Flesch Reading Ease'), 
+    'log_wcount', 'log(Word Count)'), 'log_popularity', 'log(Popularity)')
+    ) -> linear_regression_weights
 
-filter(linear_regression_weights, names!='intercept') %>%
+ggplot(filter(linear_regression_weights, names!='intercept'), aes(x=names, y=weights)) + 
+  geom_bar(stat="identity", position="dodge") + theme(axis.text.x=element_text(angle=90, size=5)) + 
+  xlab('Feature') + ylab('Feature Weight') + ggtitle('Linear Regression Feature Weights') -> p
+ggsave(p, file='feature_weights.png')
+
+linear_regression_weights %>%
+  arrange(desc(abs(weights))) %>%
+  mutate(weights = round(weights, 3)) %>%
+  dplyr::select(names, weights) %>%
+  head(n=20) %>%
   kable(., format='latex')
+
+mse_performance <- read.csv('~/Desktop/mit_6867/Project/regression/mse_performance.csv')
+
+mse_performance %>%
+  gather(group, mse, training_mse:holdout_mse) %>%
+  mutate(group = ifelse(group == 'training_mse', 'Training', 'Validation')) %>%
+  ggplot(., aes(x=lambdas, y=mse, color=group)) + geom_point(size=2) + 
+  xlab('Regularization Parameter') + ylab('MSE') + 
+  ggtitle('Effect of Regularization on MSE') -> p2
+
+ggsave(p2, file='mse_plot.png')
+
+author_data <- read.delim('../gender_guessing/authors_with_genders.tsv', sep='\t')
+
+ggplot(author_data, aes(x=popularity)) + geom_histogram() + 
+  scale_x_continuous(labels=comma, lim=c(0, 100000)) + 
+  xlab('Number of Bing Search Results') + ylab('count') -> p3
+ggsave(p3, file='author_popularity_histogram.png')
+
+ggplot(author_data, aes(x=log(popularity))) + geom_histogram() + 
+  scale_x_continuous(labels=comma, lim=c(0, 25)) + 
+  xlab('log(Number of Bing Search Results)') + ylab('count') -> p4
+ggsave(p4, file='author_popularity_log_histogram.png')
+
+flesch_data <- read.csv('../feature_extraction/readability/flesch_kincaid.txt')
+
+ggplot(flesch_data, aes(x=flesch.kincaid_score)) + geom_histogram() +
+  xlim(c(0, 120)) + xlab('Flesch Reading Ease') + ylab('count') -> p5
+ggsave(p5, file='flesch_data_histogram.png')
